831cf67a26c31f0b5e5f7087f627a0e2d3ad18ca
Alexandre Gramfort <alexandre.gramfort@m4x.org>
2016-04-27 21:48:07 +0200

    fix solution

--- notebooks/solutions/05C_validation_exercise.py
+++ notebooks/solutions/05C_validation_exercise.py
@@ -1,29 +1,34 @@
-# suppress warnings from older versions of KNeighbors
-import warnings
-warnings.filterwarnings('ignore', message='kneighbors*')
+from sklearn.datasets import load_digits
+from sklearn.model_selection import train_test_split
+from sklearn.svm import LinearSVC
+from sklearn.naive_bayes import GaussianNB
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn import metrics
+
+digits = load_digits()
 
 X = digits.data
 y = digits.target
-X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.25, random_state=0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=0)
 
 for Model in [LinearSVC, GaussianNB, KNeighborsClassifier]:
     clf = Model().fit(X_train, y_train)
     y_pred = clf.predict(X_test)
-    print(Model.__name__, metrics.f1_score(y_test, y_pred))
-    
+    print(Model.__name__, metrics.f1_score(y_test, y_pred, average='weighted'))
+
 print('------------------')
 
 # test SVC loss
-for loss in ['l1', 'l2']:
+for loss in ['hinge', 'squared_hinge']:
     clf = LinearSVC(loss=loss).fit(X_train, y_train)
     y_pred = clf.predict(X_test)
-    print("LinearSVC(loss='{0}')".format(loss), metrics.f1_score(y_test, y_pred))
-    
+    print("LinearSVC(loss='{0}')".format(loss), metrics.f1_score(y_test, y_pred, average='weighted'))
+
 print('-------------------')
-    
+
 # test K-neighbors
 for n_neighbors in range(1, 11):
     clf = KNeighborsClassifier(n_neighbors=n_neighbors).fit(X_train, y_train)
     y_pred = clf.predict(X_test)
     print("KNeighbors(n_neighbors={}): {:.3f}".format(
-        n_neighbors, metrics.f1_score(y_test, y_pred)))
+        n_neighbors, metrics.f1_score(y_test, y_pred, average='weighted')))
